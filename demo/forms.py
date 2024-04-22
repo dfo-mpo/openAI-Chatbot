008e248114a58f7b64b99a2642c041fe969ab65b
IbrahimRehman-dfo <159082030+IbrahimRehman-dfo@users.noreply.github.com>
2024-04-22 13:02:50 -0700

    Delete demo directory

--- demo/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from demo.models import CSVUpload
-
-class CSVUploadForm(forms.ModelForm):
-    class Meta:
-        model = CSVUpload
-        fields = ('csv_file',)
--- demo/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from demo import views
-
-urlpatterns = [
-    path('demo_home', views.demo_home, name='demo_home'),  # Home page
-    path('upload/', views.OpenAIView.as_view(), name='upload'),  # Upload page, using the existing OpenAIView
-    # Uncomment the next line when you're ready to implement the export functionality
-    # path('openai/export/', views.OpenAIExportView.as_view(), name='export_openai_data'),
-]
